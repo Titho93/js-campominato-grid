1ae3d9527101aa30aaf54e7f6f0a0fa336e0ed29
Titho93 <titho10.93@gmail.com>
2023-09-18 14:15:21 +0200

    [init] file html-css-js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1. seleziono il container e lo salvo in una variabile
+// 2. Creo un bottone 
+// 3. faccio un ciclo su 100
+// 4. ad ogni ciclo devo far generare un quadrato
+// 5. per ogni quadrato aggiungere un evento al click
+// 6. al click del quadrato aggiungere/togliere(toggle) la classe del click
+// 7. imposto una funzione in modo che al click del bottone venga generata la griglia
