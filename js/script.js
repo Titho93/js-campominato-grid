7a60841385ba8e7e71b0001d1fdb91a380310890
Titho93 <titho10.93@gmail.com>
2023-09-18 15:30:21 +0200

    [add]custom property for square

--- js/script.js
+++ js/script.js
@@ -10,24 +10,26 @@ reset();
 // 3. faccio un ciclo su 100
 for(let i = 1; i <= 100; i++){
   // 4.2. ad ogni ciclo devo far generare un quadrato
-  const square = addSquare();
+  const square = addSquare(i);
 
 // 6. per ogni quadrato aggiungere un evento al click
 // 7. al click del quadrato aggiungere/togliere(toggle) la classe del click
   square.addEventListener('click', function(){
+    console.log(this._squareId)
+
     this.classList.toggle('clicked');
-    console.log(this)
   })
 
   container.append(square)
 }
 
 // 4.1. creo una funzione per generare quadrati 
-function addSquare(){
+function addSquare(index){
 
   const newSquare = document.createElement('div');
   newSquare.className = 'square';
-
+  newSquare._squareId = index;
+  newSquare.innerHTML = `<span>${index}</span>`
   return newSquare;
 
 }
