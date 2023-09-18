a49d9600e6d54a19b16f8fdf2c8e3be5b0c5294f
Titho93 <titho10.93@gmail.com>
2023-09-18 15:05:18 +0200

    [add]cliclo for generate new square

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// 3. faccio un ciclo su 100
-// 4. ad ogni ciclo devo far generare un quadrato
+
+
 // 5. per ogni quadrato aggiungere un evento al click
 // 6. al click del quadrato aggiungere/togliere(toggle) la classe del click
 // 7. imposto una funzione in modo che al click del bottone venga generata la griglia
@@ -8,3 +8,22 @@
 const container   = document.querySelector('.container');
 // 2. Creo un bottone 
 const startButton = document.getElementById("start");
+
+// 3. faccio un ciclo su 100
+for(let i = 1; i <= 100; i++){
+  // 4.2. ad ogni ciclo devo far generare un quadrato
+  const square = addSquare();
+  console.log(i)
+  
+  container.append(square)
+}
+
+// 4.1. creo una funzione per generare quadrati 
+function addSquare(){
+
+  const newSquare = document.createElement('div');
+  newSquare.className = 'square';
+
+  return newSquare;
+
+}
